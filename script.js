4a5d1b3fc67fae0c0bd3285104415543372a2f28
maria_akhramovich <ahramovich2003@gmail.com>
2022-08-30 16:03:38 +0300

    todo

--- script.js
+++ script.js
@@ -1,29 +1,78 @@
-let input = document.querySelector('.input');
-let btn = document.querySelector('.btn');
-let btnDelete = document.querySelector('.btn_delete');
-let result = document.querySelector('.result');
+const addTaskBtn = document.getElementById('add-task-btn');
+const deskTaskInput = document.getElementById('text-task');
+const todosWrapper = document.querySelector('.todos-wrapper');
+showTasks();
+function showTasks() {
+    let tasks = JSON.parse(localStorage.getItem('todos')) || [];
+    let todoItemElems;
+    if (tasks == null){
+        todoItemElems = [];
+    } else{
+        todoItemElems = tasks;
+    }
+    let tasksHTML = '';
+    todoItemElems.forEach(function (task, index) {
+        tasksHTML += `
+    <div id = "${task.id}" class="todo-item ${task.completed ? "checked" : ''}">
+        <div class="text">${task.text} </div>
+        <div class="date">${task.date}</div>
+        <div class="buttons">
+            <input class="btn-complete" id="item_${index}" type="checkbox" ${task.completed ? 'checked' : ''}>
+            <button onclick="deleteTask(${index})" class="btn-delete">Delete</button>
+        </div>
+    </div>
+    `
+    })
+    if(!tasksHTML) tasksHTML = 'Please create a note';
+    todosWrapper.innerHTML = tasksHTML;
 
-btn.addEventListener('click', function () {
-   if(input.value === '') return;
-    createDelEl(input.value);
-    input.value = '';
+}
+
+function Task(text) {
+    this.id = (Math.floor(Math.random()*2022)),
+    this.date = new Date().toLocaleDateString();
+    this.text = text,
+    this.completed = false;
+}
+
+todosWrapper.addEventListener('click',function (e) {
+    let tasks = JSON.parse(localStorage.getItem('todos')) || [];
+    let target = e.target;
+    const isCheckbox = target.classList.contains('btn-complete')
+    if(isCheckbox){
+        const parent = target.parentElement.parentElement;
+        const taskId = parent.getAttribute('id')
+        changeTaskStatus(taskId, tasks);
+        localStorage.setItem('todos', JSON.stringify(tasks));
+        showTasks();
+    }
 })
-function createDelEl(value) {
 
-     const li = document.createElement('li');
-     const btn = document.createElement('button');
-     li.className = 'li';
-     li.textContent = value;
-     result.appendChild(li);
-
-     btn.className = 'btn';
-     btn.textContent = 'X';
-     li.appendChild(btn)
-
-    btn.addEventListener('click', function () {
-        result.removeChild(li);
+function changeTaskStatus(id, list){
+    list.forEach((tasks) => {
+        if (tasks.id == id){
+            tasks.completed = !tasks.completed;
+        }
     })
 }
-btnDelete.addEventListener('click', function () {
-    result.innerHTML = '';
+
+addTaskBtn.addEventListener('click', function()  {
+    let tasks = JSON.parse(localStorage.getItem('todos')) || [];
+    tasks.push(new Task(deskTaskInput.value));
+    localStorage.setItem('todos', JSON.stringify(tasks));
+    showTasks();
+    deskTaskInput.value = '';
 })
+
+let deleteAllBtn = document.getElementById('delete-task-btn');
+deleteAllBtn.addEventListener('click', function () {
+    localStorage.clear();
+    showTasks();
+})
+
+function deleteTask (index ) {
+    let tasks = JSON.parse(localStorage.getItem('todos')) || [];
+    tasks.splice(index,1)
+    localStorage.setItem('todos', JSON.stringify(tasks));
+    showTasks();
+}
